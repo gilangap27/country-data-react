de198b428543d6942fca3d8d2ca39180d4a03519
gilangap27 <harzthdev01@gmail.com>
2022-06-04 13:19:29 +0700

    Get data country from API

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Navbar from "./navbar/Navbar";
 import SearchInput from "./search-input/SearchInput";
 
 const Home = () => {
+    let [country, setCountry] = useState([]);
+
+    const eventSetCountry = (data) => {
+        setCountry(data);
+    };
+
     return (
         <>
             <Navbar />
-            <SearchInput />
+            <SearchInput onSetCountry={eventSetCountry} />
         </>
     );
 };
--- src/components/search-input/SearchInput.js
+++ src/components/search-input/SearchInput.js
@@ -1,11 +1,21 @@
 import "./SearchInput.css";
 
-const SearchInput = (props) => {
+const SearchInput = ({ onSetCountry }) => {
+    const getSearchInput = async () => {
+        const searchInput = document.querySelector("#search").value;
+        onSetCountry(await getDataAPI(searchInput));
+    }
+
+    const getDataAPI = async (country) => {
+        return await fetch("https://restcountries.com/v3.1/name/" + country)
+            .then((response) => response.json())
+    }
+
     return (
         <div className="search-data">
             <h1 className="search-title">Search Country</h1>
             <input type="text" name="search" id="search" />
-            <button className="cari-data">Cari</button>
+            <button className="cari-data" onClick={getSearchInput}>Cari</button>
         </div>
     );
 };
