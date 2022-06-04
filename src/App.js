622925ed36ceefc2088a61a0c9d47b910481a406
gilangap27 <harzthdev01@gmail.com>
2022-06-04 13:49:49 +0700

    Set title app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import Home from "./components/Home";
 
 function App() {
+  useEffect(() => {
+    window.document.title = "Country Data";
+  }, []);
+
   return (
     <div className="App">
       <Home />
